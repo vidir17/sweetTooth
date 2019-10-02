6dc22b1bceddf6662833a24447212caa1b873c4f
vidir17 <vidir17@ru.is>
2019-10-02 14:56:10 +0000

    create pinata works!

--- Services/pinataService.js
+++ Services/pinataService.js
@@ -1,6 +1,11 @@
 const db = require('../data.json');
-
-
+const request = require('request');
+const allowed = ['id', 'name', 'maximumHits'];
+const results = [];
+const surprises = ['https://vignette.wikia.nocookie.net/matrix/images/3/32/Neo.jpg/revision/latest?cb=20060715235228',
+                   'https://www.thoughtco.com/thmb/T8UuUB3R6VolLVyX9yYyGC5J3KI=/768x0/filters:no_upscale():max_bytes(150000):strip_icc()/906520-58b89ea13df78c353cce0fc1.jpg',
+                   'https://vignette.wikia.nocookie.net/matrix/images/6/67/Trinityfull.jpg/revision/latest?cb=20060803214449',
+                   'https://vignette.wikia.nocookie.net/matrix/images/4/4d/Agent-smith-the-matrix-movie-hd-wallpaper-2880x1800-4710.png/revision/latest?cb=20140504013834'];
 const globalTryCatch = async cb => {
     try{
         return await cb();
@@ -16,10 +21,6 @@ const pinataService = () => {
         array = db.pinatas;
         
         const clone = JSON.parse(JSON.stringify(array));
-        console.log(Object.getOwnPropertyNames(array[0]));
-        let allowed = ['id', 'name', 'maximumHits'];
-        const results = [];
-        
         for(var i = 0; i < db.pinatas.length; i++){
         results[i] = Object.keys(clone[i])
             .filter(id => allowed.includes(id))
@@ -27,14 +28,42 @@ const pinataService = () => {
                 obj[id] = (clone[i])[id];
                 return obj;
             }, {});
-
         }
-        
         return results;
-
+    };
+    const getPinataById = async function (Id) {  
+        var array = [];
+        Id = Id-1;
+        array = db.pinatas;
+        results[Id] = Object.keys(array[Id])
+            .filter(id => allowed.includes(id))
+            .reduce((obj, id) => {
+                obj[id] = (array[Id])[id];
+                return obj;
+            }, {});  
+    
+        return results[Id];
+    };
+    const createPinata = async function (body){
+        var FileSystem = require('fs');
+        var directory = './images';
+        request.get(surprises[Math.round(Math.random() * 3)]).pipe(FileSystem.createWriteStream('./images/' + body.name + '.jpg'));
+        if (!FileSystem.existsSync(directory)){
+            FileSystem.mkdirSync(directory);
+        }
+        var array = [];
+        array = db.pinatas;
+        var highest = Math.max(...array.map(x => x.id));
+            var newPinata = {
+                    id: highest+1,
+                    name: body.name,
+                    surprise: ('./images/' + body.name + '.jpg'),
+                    maximumHits: body.maximumHits
+                }
+        return array.push(newPinata);
     };
 
-return {getAllPinatas};
+return {getAllPinatas, getPinataById, createPinata};
 };
 
 module.exports = pinataService();
--- index.js
+++ index.js
@@ -45,6 +45,28 @@ app.get('/api/pinatas', async function (req, res){
     return res.json(result);
 });
 
+//(10%) /api/pinatas/{id} - Gets a pinata with a certain id - should contain all properties
+//excluding surprise
+app.get('/api/pinatas/:Id', async function (req, res){
+    const Id = req.params.Id;
+    const result = await pinataService.getPinataById(Id);
+    return res.json(result);
+});
+
+//(20%) /api/pinatas - Create a new pinata (NO MODEL VALIDATION) and should return the
+//newly created model along with a proper status code. Here the model should also include a
+//surprise property which can either be a written text or an URL to a valid image (.jpg, .png,
+//etc.)
+app.post('/api/pinatas', async function (req,res){
+    pinataService.createPinata(req.body).then(r => {
+    return res.status(201).json(r);
+}).catch( e => {
+    return res.status(400).json(e);
+});
+});
+
+
+
 app.listen(3000, function(){
     console.log('Server is listening on port 3000');
 });
